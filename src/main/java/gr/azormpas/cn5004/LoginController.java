73397b1f61a7c566a8b72a3ed7a9c72755221e34
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-03-21 17:48:17 +0200

    File rename

--- src/main/java/gr/azormpas/cn5004/LoginController.java
+++ src/main/java/gr/azormpas/cn5004/LoginController.java
@@ -3,7 +3,7 @@ package gr.azormpas.cn5004;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController
+public class LoginController
 {
     @FXML
     private Label welcomeText;
