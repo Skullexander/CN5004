93d2eadb9fd1eb219e7abb76cef07fa1a894730a
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-04-01 09:18:20 +0300

    Added file

---
+++ src/main/java/gr/azormpas/cn5004/controller/HomeController.java
@@ -0,0 +1,15 @@
+package gr.azormpas.cn5004.controller;
+
+import javafx.event.ActionEvent;
+
+public class HomeController
+{
+
+    public void proceedLogin(ActionEvent event)
+    {
+    }
+
+    public void testAction(ActionEvent event)
+    {
+    }
+}
