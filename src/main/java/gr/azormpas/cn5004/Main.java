d99efdc30567018153acca262a758f651b5c5f40
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-04-03 12:50:43 +0300

    Refactored scene loading

--- src/main/java/gr/azormpas/cn5004/Main.java
+++ src/main/java/gr/azormpas/cn5004/Main.java
@@ -2,23 +2,46 @@ package gr.azormpas.cn5004;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Main
     extends Application
 {
+    public static Stage stage;
+
     @Override
-    public void start(Stage stage)
+    public void start(Stage primaryStage)
         throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/Login.fxml"));
-        Scene      scene      = new Scene(fxmlLoader.load());
+        stage = primaryStage;
+
         stage.setTitle("CN5004");
-        stage.setScene(scene);
+        loadScene("Login");
         stage.show();
+    }
+
+    public static Parent loadFXML(String fxml)
+        throws IOException
+    {
+        return FXMLLoader.load(getResource(String.format("view/%s.fxml", fxml)));
+    }
+
+    public static void loadScene(String fxml)
+        throws IOException
+    {
+        Parent pane = loadFXML(fxml);
+        stage.setScene(new Scene(pane));
+    }
+
+    public static URL getResource(String path)
+    {
+        return Objects.requireNonNull(Main.class.getResource(path));
     }
 
     public static void main(String[] args)
