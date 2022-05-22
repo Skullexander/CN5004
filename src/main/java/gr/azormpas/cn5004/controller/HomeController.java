390ffea0f1181cdcb9d3e54e41b26de2db67f9bc
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-05-22 11:22:36 +0300

    Removed alerts

--- src/main/java/gr/azormpas/cn5004/controller/HomeController.java
+++ src/main/java/gr/azormpas/cn5004/controller/HomeController.java
@@ -4,71 +4,31 @@ import gr.azormpas.cn5004.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class HomeController
 {
     @FXML
     private BorderPane baseView;
 
-    Alert a = new Alert(Alert.AlertType.NONE);
     AnchorPane sideView = new AnchorPane();
-
-    public void testAction(ActionEvent event)
-    {
-        if (!Objects.equals("Test", a.getTitle())) createAlert(event);
-        a.setAlertType(Alert.AlertType.INFORMATION);
-        a.show();
-    }
-
-    public void testAction2(ActionEvent event)
-    {
-        if (!Objects.equals("Test", a.getTitle())) createAlert(event);
-        a.setAlertType(Alert.AlertType.ERROR);
-        a.show();
-    }
-
-    public void testAction3(ActionEvent event)
-    {
-        if (!Objects.equals("Test", a.getTitle())) createAlert(event);
-        a.setAlertType(Alert.AlertType.WARNING);
-        a.show();
-    }
 
     public void viewCRUD(ActionEvent ignoredEvent)
         throws IOException
     {
-        if (sideView.getChildren().isEmpty())
-        {
-            createSideView("Edit");
-        }
-        else
-        {
-            clearSideView();
-            createSideView("Edit");
-        }
+        if (!sideView.getChildren().isEmpty()) clearSideView();
+        createSideView("Edit");
         baseView.getScene().getWindow().sizeToScene();
     }
 
     public void viewList(ActionEvent ignoredEvent)
         throws IOException
     {
-        if (sideView.getChildren().isEmpty())
-        {
-            createSideView("List");
-        }
-        else
-        {
-            clearSideView();
-            createSideView("List");
-        }
+        if (!sideView.getChildren().isEmpty()) clearSideView();
+        createSideView("List");
         baseView.getScene().getWindow().sizeToScene();
     }
 
@@ -78,25 +38,11 @@ public class HomeController
         Main.loadScene("Login");
     }
 
-    private void createAlert(ActionEvent e)
-    {
-        setModality(e);
-        a.setTitle("Test");
-        a.setHeaderText("Button works!");
-        a.setContentText("This button is in working condition");
-    }
-
     private void createSideView(String view)
         throws IOException
     {
         sideView.getChildren().add(setSideView(view));
         baseView.rightProperty().set(sideView);
-    }
-
-    private void setModality (ActionEvent e)
-    {
-        a.initOwner(((Button) e.getSource()).getScene().getWindow());
-        a.initModality(Modality.WINDOW_MODAL);
     }
 
     private Node setSideView (String view)
