fc95872665cb9d100f9a5e452516bcd3eefffb14
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-05-22 11:29:02 +0300

    Implemented login function using default data

--- src/main/java/gr/azormpas/cn5004/controller/LoginController.java
+++ src/main/java/gr/azormpas/cn5004/controller/LoginController.java
@@ -1,12 +1,15 @@
 package gr.azormpas.cn5004.controller;
 
 import gr.azormpas.cn5004.Main;
+import gr.azormpas.cn5004.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
 import java.io.IOException;
 
 public class LoginController
@@ -16,24 +19,55 @@ public class LoginController
     @FXML
     private PasswordField fldPassword;
     @FXML
-    private CheckBox chkCredLogin;
+    private CheckBox chkRemember;
+    @FXML
+    private CheckBox chkDefaultData;
     @FXML
     private Button btnLogin;
+
+    private boolean isRememberChecked = false;
+    private boolean isDefaultDataChecked = false;
+    protected User loggedUser;
 
     @FXML
     public void initialize()
     {
-        btnLogin.setText("Click here...");
+        loggedUser = new User();
+        if(isRememberChecked)
+        {
+            chkRemember.setSelected(true);
+            fldUsername.setText(loggedUser.getUsername());
+            fldPassword.setText(loggedUser.getPassword());
+        }
+        if (isDefaultDataChecked)
+        {
+            chkDefaultData.setSelected(true);
+        }
+    }
+
+    public void toggleDefaultData (ActionEvent actionEvent)
+    {
+        isDefaultDataChecked = chkDefaultData.isSelected();
+    }
+
+    public void toggleRemember (ActionEvent ignoredEvent) throws IOException
+    {
+        isRememberChecked = chkRemember.isSelected();
     }
 
     public void attemptLogin(ActionEvent event) throws IOException
     {
-        //Check credentials with user DB
-
-        //If exists, check entry level
-
-        //Open "Home" scene with given entry level
-
-        Main.loadScene("Home");
+        if (Main.data.verifyUser(fldUsername.getText(), fldPassword.getText()))
+        {
+            loggedUser.setUsername(fldUsername.getText());
+            loggedUser.setPassword(fldPassword.getText());
+            loggedUser.setLastLogin(String.valueOf(new java.util.Date()));
+            Main.loadScene("Home");
+        }
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Credentials");
+            alert.show();
+        }
     }
 }
