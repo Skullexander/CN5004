1cf19608c70e5f2746f5bc69bf8159da95753325
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-05-24 11:14:18 +0300

    Added boolean variables and a User variable

--- src/main/java/gr/azormpas/cn5004/model/Settings.java
+++ src/main/java/gr/azormpas/cn5004/model/Settings.java
@@ -1,5 +1,48 @@
 package gr.azormpas.cn5004.model;
 
-public class Settings
-{}
+import java.io.Serializable;
+
+public class Settings implements Serializable
+{
+    private boolean rememberUser;
+    private boolean useDefaultData;
+    private User loadedUser;
+
+    public Settings()
+    {
+        setRememberUser(false);
+        setUseDefaultData(true);
+        loadedUser = new User();
+    }
+
+    public boolean isRememberUser()
+    {
+        return rememberUser;
+    }
+
+    public void setRememberUser(boolean rememberUser)
+    {
+        this.rememberUser = rememberUser;
+    }
+
+    public boolean isUseDefaultData()
+    {
+        return useDefaultData;
+    }
+
+    public void setUseDefaultData(boolean useDefaultData)
+    {
+        this.useDefaultData = useDefaultData;
+    }
+
+    public User getLoadedUser()
+    {
+        return loadedUser;
+    }
+
+    public void setLoadedUser(User loadedUser)
+    {
+        this.loadedUser = loadedUser;
+    }
+}
 
