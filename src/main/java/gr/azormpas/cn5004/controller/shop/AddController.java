25867cce16287232d89ae7f71f75e413ec244f08
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-05-25 04:17:58 +0300

    Added functionality to create new products

--- src/main/java/gr/azormpas/cn5004/controller/shop/AddController.java
+++ src/main/java/gr/azormpas/cn5004/controller/shop/AddController.java
@@ -12,4 +12,47 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AddController
-{}
+{
+    @FXML
+    private TextField fldName, fldCost;
+    @FXML
+    private TextArea fldInfo;
+    @FXML
+    private CheckBox chkIsAvailable;
+    @FXML
+    private Button btnCancel;
+
+    public void addProduct(ActionEvent ignoredEvent)
+    {
+        if (fldName.getText().isBlank())
+        {
+            new Alert(AlertType.ERROR, "Product name is empty or blank.").show();
+        }
+        else if (fldCost.getText().isBlank() && fldCost.getText().matches("^\\d*\\.\\d{2}$"))
+        {
+            new Alert(AlertType.ERROR, "Product cost invalid. Mandatory use of exactly two decimals (example: 12.00)").show();
+        }
+        else
+        {
+            Main.data.getShops()
+                     .get(Main.data.getUserLocation(Main.data.getSettings().getLoadedUser().getUsername()))
+                     .getInventory()
+                     .add(new Product(
+                         Main.data.getShops().get(Main.data.getUserLocation(Main.data.getSettings().getLoadedUser().getUsername())),
+                         fldName.getText(),
+                         Double.parseDouble(fldCost.getText()),
+                         chkIsAvailable.isSelected()));
+
+            Main.data.getShops()
+                     .get(Main.data.getUserLocation(Main.data.getSettings().getLoadedUser().getUsername()))
+                     .getInventory()
+                     .get(Main.data.getShops().get(Main.data.getUserLocation(Main.data.getSettings().getLoadedUser().getUsername())).getInventory().size()-1)
+                     .setInfo(fldInfo.getText());
+        }
+    }
+
+    public void cancelAdd(ActionEvent ignoredEvent)
+    {
+        btnCancel.getScene().getWindow().hide();
+    }
+}
