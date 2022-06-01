965870450ba51694b09e12b3d52663b2a9af4205
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-06-01 12:34:15 +0300

    Updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,6 @@ module gr.azormpas.cn5004 {
     exports gr.azormpas.cn5004;
     exports gr.azormpas.cn5004.controller;
     opens gr.azormpas.cn5004.controller to javafx.fxml;
-    exports gr.azormpas.cn5004.controller.shop;
-    opens gr.azormpas.cn5004.controller.shop to javafx.fxml;
+    exports gr.azormpas.cn5004.controller.product;
+    opens gr.azormpas.cn5004.controller.product to javafx.fxml;
 }
