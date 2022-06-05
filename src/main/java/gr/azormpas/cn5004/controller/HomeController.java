819a469887395545b937914b09414a63970fab11
Alexandros <68503305+Skullexander@users.noreply.github.com>
2022-06-05 21:45:10 +0300

    Removed unused elements

--- src/main/java/gr/azormpas/cn5004/controller/HomeController.java
+++ src/main/java/gr/azormpas/cn5004/controller/HomeController.java
@@ -3,12 +3,9 @@ package gr.azormpas.cn5004.controller;
 import gr.azormpas.cn5004.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -16,16 +13,12 @@ import java.io.IOException;
 public class HomeController
 {
     @FXML
-    private BorderPane baseView;
-    @FXML
     private VBox boxOptions;
     @FXML
     private Label txtName;
     @FXML
     private Button btnProfile, btnUserList, btnShopList, btnCustomerList, btnProductList, btnPurchaseList;
-    private final AnchorPane sideView = new AnchorPane();
     private String type;
-    private Alert alert;
 
     public void initialize()
     {
@@ -145,31 +138,5 @@ public class HomeController
             Main.data.saveData();
         }
         Main.loadScene("Login");
-    }
-
-
-    private void createSideView(String view)
-        throws IOException
-    {
-        if (!sideView.getChildren().isEmpty()) clearSideView();
-        sideView.getChildren().add(setSideView(String.format("%s/%s",type ,view)));
-        baseView.rightProperty().set(sideView);
-        resizeView();
-    }
-
-    private Node setSideView(String view)
-        throws IOException
-    {
-        return Main.loadFXML(view);
-    }
-
-    private void clearSideView()
-    {
-        sideView.getChildren().clear();
-    }
-
-    private void resizeView()
-    {
-        baseView.getScene().getWindow().sizeToScene();
     }
 }
